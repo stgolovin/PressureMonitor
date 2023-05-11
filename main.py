d0bbf0c77841d5ff6806ba489b3e1be003f03a1b
Stanislav Golovin <golovin.ds93@gmail.com>
2023-05-11 16:49:59 +0300

    add diagram

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-
+import matplotlib.pyplot as plt
 from datetime import datetime
 
 from models import User, Measurement, create_tables
 
 LOGIN = 'postgres'
-PASSWORD = input('Password: ')
+PASSWORD = 'MPuzo1920'
 DB = 'pm_db'
 
 DSN = f"postgresql://{LOGIN}:{PASSWORD}@localhost:5432/{DB}"
@@ -35,6 +35,25 @@ def get_measurements(_user_id):
     for m in session.query(Measurement).join(User.measurements).filter(User.id == _user_id).all():
         print(m)
 
+def show_diagramm(_user_id):
+    hp_list = []
+    lp_list = []
+    dates = []
+    for m in session.query(Measurement).join(User.measurements).filter(User.id == _user_id).all():
+        hp_list.append(m.hp)
+        lp_list.append(m.lp)
+        dates.append(str(m.date.date()))
+    plt.style.use('bmh')
+    fig, ax = plt.subplots()
+    ax.plot(dates, hp_list, c='blue', linewidth=2)
+    ax.plot(dates, lp_list, c='red', linewidth=2)
+    ax.set_title("HP and LP measurements", fontsize=18)
+    ax.set_xlabel('Dates', fontsize=14)
+    fig.autofmt_xdate()
+    ax.set_ylabel('Pressure', fontsize=14)
+    ax.tick_params(axis='both', labelsize=11)
+    plt.show()
+
 
 session.close()
 
@@ -43,9 +62,9 @@ def main():
     now = datetime.now()
     # create_user(name=input("Как Вас зовут? "), lastname=input("Какая у Вас фамилия? "))
     # create_measurement(user_id=input("Укажите ID пользователя: "), date=now, hp=input("Введите верхнее давление: "), lp=input("Введите нижнее давление: "), heart_rate=input("Укажите частоту сердцебиения: "))
-    get_users()
+    # get_users()
     # get_measurements(_user_id=input("Укажите ID пользователя: "))
-
+    # show_diagramm(_user_id=input("Укажите ID пользователя: "))
 
 if __name__ == "__main__":
     main()
